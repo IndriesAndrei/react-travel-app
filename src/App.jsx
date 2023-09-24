ca3b5131a510716be9033308f0973e03a9f62f32
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-09-24 18:29:21 +0300

    Last finishes for the project

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,21 @@ function App() {
     ))
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm('Are you sure you want to delete all items?')
+    if (confirmed) setItems([]);
+  }
+
   return (
     <>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingLIst items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+      <PackingLIst 
+        items={items} 
+        onDeleteItem={handleDeleteItem} 
+        onToggleItem={handleToggleItem} 
+        onClearList={handleClearList}
+      />
       <Stats items={items} />
     </>
   )
